8b8f88a8efa019369b49c880100efecd8c74f527
sted1as <stefanydiasrossi@gmail.com>
2024-06-05 09:04:59 -0300

    aula de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        // a classe String que representa texto na aplicação
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        double VALOR_DE_PI = 3.14;
+
+        
     }
 }

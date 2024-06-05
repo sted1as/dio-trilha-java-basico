97cfb9e0359621dc999c5fa66e6f37723121d8dc
sted1as <stefanydiasrossi@gmail.com>
2024-06-05 11:10:18 -0300

    exemplos da aula sobre operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,46 @@
+public class Operadores {
+    public static void main(String[] args) {
+        
+    }
+    //Operadores de Atribuição
+//String nome = "STEFANY"; //texto precisa ser definido com "" para exatidão
+//int idade = 22;
+//double peso = 68.5; //não esquecer da convenção de que quando o número é quebrado, tem que usar .
+//char sexo = 'M';
+//boolean doadorOrgao = false; //boolean é definido como true ou false
+//Date dataNascimento = new Date();
+
+    //Operadores Artitméticos
+//double soma = 10.5 + 15.7;
+//int subtração = 113 - 25;
+//int multiplicacao = 20 * 7;
+//int divisao = 15 / 3;
+//int modulo = 18 % 3; //forma de verificar se a divisão da expressão resultará números fracionados(restos)
+//double resultado = (10 * 7) + (20/4); //mix de uso de operadores
+//String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+    //Operadores Unários
+//int numero = 5;
+    //x repetição
+// numero++;
+    //numero = numero + 1
+
+    //Operadores Ternários
+//int a, b;
+//a = 7;
+//b = 6;
+//String resultado = a==b ? "verdadeiro" : "falso";
+
+    //Operadores relacionais
+//int numero1 = 1;
+//int numero2 = 2;
+//boolean simNao = numero1 == numero2;
+//System.out.println("numeroUm é igual a numeroDois? " + simNao);
+ //simNao = numero1 != numero2;
+//System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+//simNao = numero1 > numero2;
+//System.out.println("numeroUm é maior a numeroDois? " + simNao);
+
+    //Operadores Lógicos
+}
+
